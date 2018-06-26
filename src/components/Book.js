1608e828812e10363e722e624a269f720802be18
mbedree <mabedree@gmail.com>
2018-06-26 14:05:32 -0400

    Use the BookList and Book componenets to render the Want to Read and Read books

--- src/components/Book.js
+++ src/components/Book.js
@@ -71,3 +71,101 @@ export default Book;
 //     <div className="book-authors">{props.books[1].author}</div>
 //   </div>
 // </li>
+// <div className="bookshelf-books">
+//   <ol className="books-grid">
+//     <li>
+//       <div className="book">
+//         <div className="book-top">
+//           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: props.books[2].imgURL }}></div>
+//           <div className="book-shelf-changer">
+//             <select>
+//               <option value="move" disabled>Move to...</option>
+//               <option value="currentlyReading">Currently Reading</option>
+//               <option value="wantToRead">Want to Read</option>
+//               <option value="read">Read</option>
+//               <option value="none">None</option>
+//             </select>
+//           </div>
+//         </div>
+//         <div className="book-title">{props.books[2].name}</div>
+//         <div className="book-authors">{props.books[2].author}</div>
+//       </div>
+//     </li>
+//     <li>
+//       <div className="book">
+//         <div className="book-top">
+//           <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: props.books[3].imgURL }}></div>
+//           <div className="book-shelf-changer">
+//             <select>
+//               <option value="move" disabled>Move to...</option>
+//               <option value="currentlyReading">Currently Reading</option>
+//               <option value="wantToRead">Want to Read</option>
+//               <option value="read">Read</option>
+//               <option value="none">None</option>
+//             </select>
+//           </div>
+//         </div>
+//         <div className="book-title">{props.books[3].name}</div>
+//         <div className="book-authors">{props.books[3].author}</div>
+//       </div>
+//     </li>
+//   </ol>
+// </div>
+// <div className="bookshelf-books">
+//   <ol className="books-grid">
+//     <li>
+//       <div className="book">
+//         <div className="book-top">
+//           <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: props.books[4].imgURL }}></div>
+//           <div className="book-shelf-changer">
+//             <select>
+//               <option value="move" disabled>Move to...</option>
+//               <option value="currentlyReading">Currently Reading</option>
+//               <option value="wantToRead">Want to Read</option>
+//               <option value="read">Read</option>
+//               <option value="none">None</option>
+//             </select>
+//           </div>
+//         </div>
+//         <div className="book-title">{props.books[4].name}</div>
+//         <div className="book-authors">{props.books[4].author}</div>
+//       </div>
+//     </li>
+//     <li>
+//       <div className="book">
+//         <div className="book-top">
+//           <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: props.books[5].imgURL }}></div>
+//           <div className="book-shelf-changer">
+//             <select>
+//               <option value="move" disabled>Move to...</option>
+//               <option value="currentlyReading">Currently Reading</option>
+//               <option value="wantToRead">Want to Read</option>
+//               <option value="read">Read</option>
+//               <option value="none">None</option>
+//             </select>
+//           </div>
+//         </div>
+//         <div className="book-title">{props.books[5].name}</div>
+//         <div className="book-authors">{props.books[5].author}</div>
+//       </div>
+//     </li>
+//     <li>
+//       <div className="book">
+//         <div className="book-top">
+//           <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: props.books[6].imgURL }}></div>
+//           <div className="book-shelf-changer">
+//             <select>
+//               <option value="move" disabled>Move to...</option>
+//               <option value="currentlyReading">Currently Reading</option>
+//               <option value="wantToRead">Want to Read</option>
+//               <option value="read">Read</option>
+//               <option value="none">None</option>
+//             </select>
+//           </div>
+//         </div>
+//         <div className="book-title">{props.books[6].name}</div>
+//         <div className="book-authors">{props.books[6].author}</div>
+//       </div>
+//     </li>
+//   </ol>
+// </div>
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -19,107 +19,17 @@ const BookShelf = props => {
           </div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: props.books[2].imgURL }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{props.books[2].name}</div>
-                    <div className="book-authors">{props.books[2].author}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: props.books[3].imgURL }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{props.books[3].name}</div>
-                    <div className="book-authors">{props.books[3].author}</div>
-                  </div>
-                </li>
-              </ol>
-            </div>
+            <BookList
+              books={props.books}
+              shelf={"Want"}
+            />
           </div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: props.books[4].imgURL }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{props.books[4].name}</div>
-                    <div className="book-authors">{props.books[4].author}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: props.books[5].imgURL }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{props.books[5].name}</div>
-                    <div className="book-authors">{props.books[5].author}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: props.books[6].imgURL }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{props.books[6].name}</div>
-                    <div className="book-authors">{props.books[6].author}</div>
-                  </div>
-                </li>
-              </ol>
-            </div>
+            <BookList
+              books={props.books}
+              shelf={"Read"}
+            />
           </div>
         </div>
         <div className="open-search">
