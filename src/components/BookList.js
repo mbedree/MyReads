962d60d4fcd9fdb7353b6cedd791f1343f6aee80
mbedree <mabedree@gmail.com>
2018-06-26 13:49:02 -0400

    Move the render of the Currently Reading books to BookList and map over props.books calling Book.js to render each book

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Book from './Book';
 
 const BookList = props => {
   return (
-    
-  );
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {props.books.map(book) => (
+          <Book
+            book={book}
+            shelf={props.shelf}
+          />
+        )}
+      </ol>
+    </div>
+  )
 }
 
 export default BookList;
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import BookList from './BookList';
 
 const BookShelf = props => {
   return (
@@ -11,46 +12,10 @@ const BookShelf = props => {
         <div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: props.books[0].imgURL }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{props.books[0].name}</div>
-                    <div className="book-authors">{props.books[0].author}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: props.books[1].imgURL }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{props.books[1].name}</div>
-                    <div className="book-authors">{props.books[1].author}</div>
-                  </div>
-                </li>
-              </ol>
-            </div>
+            <BookList
+              books={props.books}
+              shelf={"Current"}
+            />
           </div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Want to Read</h2>
