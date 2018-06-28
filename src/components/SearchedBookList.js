320652007cfe4ca6986663ac6ebdcca91884ec1a
mbedree <mabedree@gmail.com>
2018-06-28 16:37:24 -0400

    uUse typeof to check if the book has all the proerties needed

--- src/components/SearchedBookList.js
+++ src/components/SearchedBookList.js
@@ -13,12 +13,15 @@ class SearchedBookList extends Component {
   }
 
   render(){
+    const thumbnail = typeof this.props.book.imageLinks === "undefined" ? "" : this.props.book.imageLinks.thumbnail;
+    const title = typeof this.props.book.title === "undefined" ? "" : this.props.book.title;
+    const author = typeof this.props.book.authors === "undefined" ? "" : this.props.book.authors;
     return(
       <div>
         <li>
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }}></div>
               <div className="book-shelf-changer">
               <select
                 value="none"
@@ -31,8 +34,8 @@ class SearchedBookList extends Component {
                 </select>
               </div>
             </div>
-            <div className="book-title">{this.props.book.title}</div>
-            <div className="book-authors">{this.props.book.authors}</div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{author}</div>
           </div>
         </li>
       </div>
